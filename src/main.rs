4a11da61cac779023ecdd5aa96b6099ccd94296e
projectfinalaudio <irfanghat@gmail.com>
2023-11-03 20:08:01 +0300

    11/3/23 -> Added infinite loop

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ use std::io::{self, Read, Write};
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
-    let mut buffer = [0; CHUNK_SIZE];
-    let num_read = io::stdin().read(&mut buffer).unwrap();
-    eprintln!("num_read: {}", num_read);
-    io::stdout().write_all(&buffer[..num_read]).unwrap();
+    let mut total_bytes = 0;
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(bytes) => bytes,
+            Err(_) => break
+        };
+        total_bytes += num_read;
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    eprintln!("Total bytes: {}", total_bytes);
 }
