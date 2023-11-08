5d7b7e2171ca9f3ee4376b243d54dd3c41adde93
projectfinalaudio <irfanghat@gmail.com>
2023-11-08 11:02:53 +0300

    11/8/23 -> Configured pre-commit hook with cargo fmt & cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ const CHUNK_SIZE: usize = 16 * 1024;
 fn main() {
     // Create a new String in case there's an error -> unwrap_or(String...)
     // If the length is > 0, then environment variable is present and contains a value
-    let silent = env::var("PV_SILENT").unwrap_or(String::new()).len() > 0;
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
     // Preview silent using the dbg! macro -> one step above println -> it takes any arbitrary expression
     // dbg!(silent);
     let mut total_bytes = 0;
@@ -17,7 +17,7 @@ fn main() {
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(bytes) => bytes,
-            Err(_) => break
+            Err(_) => break,
         };
         total_bytes += num_read;
         io::stdout().write_all(&buffer[..num_read]).unwrap();
