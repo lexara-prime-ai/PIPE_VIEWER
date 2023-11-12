2bbb21aab010942f942c06dd074fcbd634ba7ea3
projectfinalaudio <irfanghat@gmail.com>
2023-11-12 15:38:35 +0300

    Added logic to calculate rate per second

--- src/stats.rs
+++ src/stats.rs
@@ -5,18 +5,24 @@ use std::time::Instant;
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
+    let mut last_instant = Instant::now();
     loop {
         // Receive bytes/buffer from read thread
         let num_bytes = stats_rx.recv().unwrap();
+        let now = Instant::now();
+        // Calculate rate per second
+        let rate_per_second = num_bytes as f64 / (now - last_instant).as_secs_f64();
+        last_instant = now;
         total_bytes += num_bytes;
 
         // Update console output
         if !silent {
             // Use carriage return '\r' to return the cursor to beginning of the line
             eprint!(
-                "\rTotal bytes: {} Time taken: {}",
+                "\rTotal bytes: {} Time taken: {} Bytes per sec: [{:.0}b/s]",
                 total_bytes,
-                start.elapsed().as_secs()
+                start.elapsed().as_secs(),
+                rate_per_second
             );
         }
         // Check if there's a need to quit, if so break out of the loop
