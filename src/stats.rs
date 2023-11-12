8ff819063ea54f62de6a6cb6c9b13dcc82bce3e7
projectfinalaudio <irfanghat@gmail.com>
2023-11-12 16:47:56 +0300

    Added custom trait implementation for time formatting

--- src/stats.rs
+++ src/stats.rs
@@ -1,27 +1,29 @@
 use crossbeam::channel::Receiver;
 use std::io::Result;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
-    let mut last_instant = Instant::now();
+    let mut timer = Timer::new();
     loop {
         // Receive bytes/buffer from read thread
         let num_bytes = stats_rx.recv().unwrap();
-        let now = Instant::now();
+        timer.update();
         // Calculate rate per second
-        let rate_per_second = num_bytes as f64 / (now - last_instant).as_secs_f64();
-        last_instant = now;
+        let rate_per_second = num_bytes as f64 / timer.delta.as_secs_f64();
+
         total_bytes += num_bytes;
 
         // Update console output
-        if !silent {
+        if !silent && timer.ready {
+            timer.ready = false;
             // Use carriage return '\r' to return the cursor to beginning of the line
             eprint!(
                 "\rTotal bytes: {} Time taken: {} Bytes per sec: [{:.0}b/s]",
                 total_bytes,
-                start.elapsed().as_secs(),
+                // Implement custom trait
+                start.elapsed().as_secs().as_time(),
                 rate_per_second
             );
         }
@@ -34,4 +36,55 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
         eprintln!();
     }
     Ok(())
+}
+
+// Custom trait implementation for u64
+trait TimeOutput {
+    fn as_time(&self) -> String;
+}
+
+impl TimeOutput for u64 {
+    fn as_time(&self) -> String {
+        // Divide by 3600 and mod i.e get Remainder by 3600
+        // to get the amount of Hours and Minutes
+        let (hours, left) = (*self / 3600, *self % 3600);
+        // Perform calculation for minutes
+        let (minutes, seconds) = (left / 60, left % 60);
+        // Format hours, minutes & seconds
+        // Make sure minutes & seconds are 0 padded
+        format!("{}:{:02}:{:02}", hours, minutes, seconds)
+    }
+}
+
+struct Timer {
+    last_instant: Instant,
+    delta: Duration,
+    period: Duration,
+    countdown: Duration,
+    ready: bool,
+}
+
+impl Timer {
+    fn new() -> Self {
+        let now = Instant::now();
+        Self {
+            last_instant: now,
+            delta: Duration::default(),          // Default value -> 0
+            period: Duration::from_millis(1000), // Timing 1s
+            countdown: Duration::default(),      // Default value -> 0
+            ready: true,                         // Set to true to get immediate progress output
+        }
+    }
+
+    fn update(&mut self) {
+        let now = Instant::now();
+        self.delta = now - self.last_instant;
+        self.last_instant = now;
+        // Decrement countdown
+        // Durations can't be  negative -> need to use checked_sub to return a Result
+        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
+            self.ready = true;
+            self.period
+        });
+    }
 }
